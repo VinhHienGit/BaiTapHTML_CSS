a0589ce8a341857b554669dd01bc7d71c683b5d2
hienz <nguyenzone95@gmail.com>
2018-09-25 10:21:10 +0700

    Update menu

--- DE 1/ThucHanh1.php
+++ DE 1/ThucHanh1.php
@@ -21,10 +21,40 @@
 	<dir class="inner">
 		<div class="content">
 			<h3 class="title">Our product = Your Success</h3>
+			<p>
+				Want to know how to get the most from your website? Here at Surefire we're experts at helping businesses do just that. We design great looking websites that are clear and intuitive. Don't just take our word for it though ? see for yourself We offer our services for over 300 TV shows spread around channels from all around the world so we are bound to be able to find and cater to your viewing needs. If we dont have what you were looking for right now then dont worry, we are continuously expanding our database - you can even submit TV shows you would recommend we add!
+			</p>
 		</div>
+		<img class="img-intro" src="img/home_intro_image.jpg" alt="Home_intro">
 	</dir>
 </dir>
 <!--Menu -->
+<dir class="menu">
+	<div class="inner">
+		<ul>
+			<li>
+				<a href="#">
+					<img src="img/port_thumb_point_progress.jpg" alt="Point_progress">
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<img src="img/port_thumb_giant_impact.gif" alt="Giant_impact">
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<img src="img/port_thumb_marsh_johnson.jpg" alt="Marsh_johnson">
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<img src="img/port_thumb_vivio.jpg" alt="Vivio">
+				</a>
+			</li>
+		</ul>
+	</div>
+</dir>
 <!--Main -->
 <!--Fooder -->
 </body>
