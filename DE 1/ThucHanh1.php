d95127bf0fc11f2e9e438c4de7de8c01c4093ff6
hienz <nguyenzone95@gmail.com>
2018-09-25 09:08:52 +0700

    Update ThucHanh1

---
+++ DE 1/ThucHanh1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<title>RUNSYSTEM</title>
+	<link rel="stylesheet" href="styles_th1.css">
+</head>
+<body>
+<!--Header -->
+<div id="header">
+	<div class="inner">
+	<a href="#">
+		<img src="img/logo1.gif" alt="LOGO">
+	</a>
+  </div>
+</div>
+<!--Banner -->
+<dir class="banner">
+	<dir class="inner">
+		<div class="content">
+			<h3 class="title">Our product = Your Success</h3>
+		</div>
+	</dir>
+</dir>
+<!--Menu -->
+<!--Main -->
+<!--Fooder -->
+</body>
+</html>
