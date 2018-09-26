297d071e9dfbcc8ca638e49d845e5b2739ebb929
hienz <nguyenzone95@gmail.com>
2018-09-26 11:13:21 +0700

    LyThuyet2 okeey.

--- DE 3/LyThuyet.php
+++ DE 3/LyThuyet.php
@@ -15,20 +15,66 @@
 	<div class="post">
 		<div class="content">
 	    <img src="img/group.jpg" alt="Group">
-      <h4>Phần này là tiêu đề của khối, chữ màu trắng</h4>
+      <p id="title">Phần này là tiêu đề của khối, chữ màu trắng.</p>
       <p>Phông chữ của cả trang là arial với cỡ chữ là 0.6875cm. Trừ những thẻ đặc biệt chữ có màu #595959. Các liên kết được làm nổi bật bằng cách tô màu trắng. Tuy nhiên lại ko có gạch chân ở dưới liên kết. Ảnh minh họa của khối này là group.jpg.Ảnh có kích thước thật 300px*150px .Trong khối này thì chỉ hiển thị với kích thước 200px * 100px.<br>
       Hãy thể hiện khối ảnh trên thành hai phần như trên.. còn khối này chỉ một mà thôi</p>
     </div>
+    <dir class="more-info">
     <a href="#">more info</a>
+    </dir>
 	</div>
   <!-- Post 2 -->
   <dir class="post">
-    bla bla
+  	<div class="content">  
+      <p id="title">Hãy thể hiện khối tin này với style giống khối trên</p>
+  	  <p id="post-2">Và bên dưới khối này thông tinđược chia đôi, hãy hiển thị đúng</p>
+      <ul>
+      	<li>
+      		<a href="#">Quisque nullalibero nis l portserisque</a>
+      	</li>
+      	<li>
+      		<a href="#">Eget males uada at nequivamus</a>
+      	</li>
+      	<li>
+      		<a href="#">Eget nibhtiam ours us leo vel metus er</a>
+      	</li>
+      	<li>
+      		<a href="#">Nulla facilisienean nec er os estibulum</a>
+      	</li>
+      </ul>
+      <ul>
+      	<li>
+      		<a href="#">Opget, malesuada at, nequivamus</a>
+      	</li>
+      	<li>
+      		<a href="#">Eget nibhtiam ours us leo vel metus er</a>
+      	</li>
+      	<li>
+      		<a href="#">Nulla facilisienean nec er os estibulum</a>
+      	</li>
+      	<li>
+      		<a href="#">ante ips um primis in faucibus or ci</a>
+      	</li>
+      </ul>
+    </div>
+      <dir class="more-info">
+        <a href="#">more info</a>
+      </dir>
   </dir>
   </div>
 <!--page right-->
   <div class="page-right">
-	
+	<div class="content"></div>
+	  <img src="img/E.gif" alt="E.gif">
+	  <p id="title">Đây là khối thông tin bên phải</p>
+	  <p>Ảnh phía bên trên là E.gif</p>
+	  <p style="font-size: 13px">Màu nền của khối là #232326</p>
+	  <p id="title" class="float-left">Aenean nec eros.</p>
+	  <p>Vesti bulum ante</p>
+	  <p style="font-size: 12px; height: 40px;">Ảnh của dòng more info là info.gif</p>
+	  <dir class="more-info">
+        <a href="#">more info</a>
+      </dir>
   </div>
 </div>
 </body>
